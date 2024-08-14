ca72c4a466de905736cb9245e102e974380b656b
Ahmed Abdelhailem <ahmedabdelhaliem0@gmail.com>
2024-08-14 15:21:03 +0300

    SPLASH view

---
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -0,0 +1,22 @@
+import 'package:book_ly/core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(
+            height: 100,
+            width: 200,
+            child: Image.asset(
+              AssetsData.logo,
+            ))
+      ],
+    );
+  }
+}
