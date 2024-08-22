a248e03b6569035f532c2fb41529ef4850402639
Ahmed Abdelhailem <ahmedabdelhaliem0@gmail.com>
2024-08-22 18:10:03 +0300

    regester book entity adabtor and open box adaptor

--- lib/constant.dart
+++ lib/constant.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const kPrimarycolor = Color(0xff150526);
 const kTranstionDuration = Duration(milliseconds: 5);
 const kGTSectraFine = 'GT-Sectra-Fine';
+const kFeaturedBox = 'Feature_dBox';
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:book_ly/constant.dart';
 import 'package:book_ly/core/utils/app_router.dart';
+import 'package:book_ly/features/home/domain/entites/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
 
-void main() {
-  
+void main() async {
   runApp(const Bookly());
-  Hive.
+  Hive.registerAdapter(BookEntityAdapter());
+  await Hive.openBox(kFeaturedBox);
 }
 
 class Bookly extends StatelessWidget {
